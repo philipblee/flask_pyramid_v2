1d4205af87544c08e7a6eeab6a66eae4bfd97dfc
Philip Lee <philipblee@gmail.com>
2022-08-07 09:40:41 -0400

    Added sort

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from Deck import *
 from flask import Flask, render_template
 from sort_cards import *
 
+# submit button
+# login
+# sort by suit and rank
 
 app = Flask(__name__)
 
@@ -17,9 +20,11 @@ def index():
 @app.route('/new_hand')
 def new_hand():
     show_next_hand()
-    global twentyfive_cards
+    global twentyfive_cards, rank_sorted_cards, suit_sorted_cards
     twenty_cards = (sorted(twentyfive_cards[0][0:20], key=rank_sort, reverse=True))
     kitty_cards = (sorted(twentyfive_cards[0][20:25], key=rank_sort, reverse=True))
+    rank_sorted_cards = (sorted(twentyfive_cards[0][0:25], key=rank_sort, reverse=True))
+    suit_sorted_cards = (sorted(twentyfive_cards[0][0:25], key=suit_rank_sort, reverse=True))
     twentyfive_cards = twenty_cards + kitty_cards
     print (twentyfive_cards)
     card_path = []
@@ -33,7 +38,27 @@ def show_kitty():
     card_path = []
     for card in twentyfive_cards:
         card_path.append("static/Cards_gif/" + card[0:2] + ".gif")
-    return render_template("show_kitty.html", card_list=twentyfive_cards, card_path=card_path)
+    return render_template("sorted.html", card_list=twentyfive_cards, card_path=card_path)
+
+@app.route('/sort_by_rank')
+def sort_by_rank():
+    global twentyfive_cards, rank_sorted_cards, suit_sorted_cards
+    print ("sort by rank")
+    card_path = []
+    print (rank_sorted_cards)
+    for card in rank_sorted_cards:
+        card_path.append("static/Cards_gif/" + card[0:2] + ".gif")
+    return render_template("sorted.html", card_list=rank_sorted_cards, card_path=card_path)
+
+@app.route('/sort_by_suit')
+def sort_by_suit():
+    global twentyfive_cards, rank_sorted_cards, suit_sorted_cards
+    print ("sort by suit")
+    print (suit_sorted_cards)
+    card_path = []
+    for card in suit_sorted_cards:
+        card_path.append("static/Cards_gif/" + card[0:2] + ".gif")
+    return render_template("sorted.html", card_list=suit_sorted_cards, card_path=card_path)
 
 if __name__ == '__main__':
     app.run(debug=True)
